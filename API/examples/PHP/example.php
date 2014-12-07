5af9fa42353b4a732247d4108d522e1722ee98b5
dostapiuk <dostapiuk@DOstapiuk-MBPro.local>
2014-12-07 22:20:11 +0100

    php client

--- API/examples/PHP/example.php
+++ API/examples/PHP/example.php
@@ -1,96 +1,99 @@
 <?php
 
 try {
-	include_once './restClient.php';
-    $client = new restClient(array('api_key' => 'MY_API_KEY'));
+	include_once './ClickMeetingRestClient.php';
+    $client = new ClickMeetingRestClient(array('api_key' => 'MY_API_KEY'));
 
-    // new conference data
+    // Conferences
     $params = array(
-  		'name' => 'test_room', // room name
-  		'room_type' => 'meeting', // room type
-  		'permanent_room' => 0, // permanent or time scheduled room
-  		'access_type' => 3, // room access type, 1 – open to all
+  		'name' => 'test_room',
+  		'room_type' => 'meeting',
+  		'permanent_room' => 0,
+  		'access_type' => 3,
   		'registration' => array('enabled' => true),
 	);
     
     $conference = $client->addConference($params);
-    $conference_id = $conference->room->id;
+    $room_id = $conference->room->id;
     
     print_r($conference);
 
-    print_r($client->conference($conference_id));
+    print_r($client->conference($room_id));
     
     print_r($client->conferences());
 
-    $params['name'] = 'new_test_room';
-    print_r($client->editConference($conference_id, $params));
+    print_r($client->editConference($room_id, array('name' => 'new_test_room')));
 
-    print_r($client->conferenceAutologinHash($conference_id, array(
+    print_r($client->conferenceAutologinHash($room_id, array(
         'email' => 'email@domain.com',
         'nickname' => 'my_nickname',
-        'role' => 'listener',
-    )));
+        'role' => 'listener'))
+    );
     
-    $params = array('how_many' => 2);
-    print_r($client->generateConferenceTokens($conference_id, $params));
+    print_r($client->sendConferenceEmailInvitations($room_id, 'us', array(
+    	'attendees' => array(
+    		array('email' => 'example@domain.com'))))
+    );
     
-    print_r($client->conferenceTokens($conference_id));
+    // Tokens
+    print_r($client->generateConferenceTokens($room_id, array('how_many' => 2)));
     
-    print_r($client->conferenceSessions($conference_id));
+    print_r($client->conferenceTokens($room_id));
     
-    $session_id = 'MY_SESSION_ID';
-    print_r($client->conferenceSession($conference_id, $session_id));
+    // Sessions
+    print_r($client->conferenceSessions($room_id));
     
-    print_r($client->conferenceSessionAttendees($conference_id, $session_id));
+    $existing_room_id = 123;
+    $existing_session_id = 456;
+    print_r($client->conferenceSession($existing_room_id, $existing_session_id));
     
-    print_r($client->generateConferenceSessionPDF($conference_id, $session_id, 'en'));
+    print_r($client->conferenceSessionAttendees($existing_room_id, $existing_session_id));
     
+    print_r($client->generateConferenceSessionPDF($existing_room_id, $existing_session_id, 'en'));
+    
+    // Timezones
     print_r($client->timeZoneList());
     
     print_r($client->countryTimeZoneList('en'));
     
-    $params = array(
-    		'registration' => array(
-    				1 => 'John',
-    				2 => 'Dee',
-    				3 => 'example@domain.com',
-    		),
+    // Registrations
+    print_r($client->addConferenceRegistration($room_id, array('registration' => array(
+    	1 => 'John',
+    	2 => 'Dee',
+    	3 => 'example@domain.com')))
     );
     
-    print_r($client->addConferenceRegistration($conference_id, $params));
+    print_r($client->conferenceRegistrations($room_id, 'all'));
     
-    print_r($client->conferenceRegistrations($conference_id, 'all'));
+    // File library
+    $file = $client->addFileLibraryFile('/my/file.png');
+    print_r($file);
+    $file_id = $file->id;
     
     print_r($client->fileLibrary());
     
-    print_r($client->conferenceFileLibrary($conference_id));
+    print_r($client->conferenceFileLibrary($room_id));
     
-    $file_id = 'MY_FILE_ID';
     print_r($client->fileLibraryFile($file_id));
     
    	print_r($client->fileLibraryContent($file_id));
-
-    print_r($client->conferenceRecordings($conference_id));
+   	
+   	print_r($client->deleteFileLibraryFile($file_id));
+   	
+	// Recordings
+    print_r($client->conferenceRecordings($room_id));
     
-    print_r($client->deleteConferenceRecordings($conference_id));
+    print_r($client->deleteConferenceRecordings($room_id));
     
-    $recording_id = 'MY_RECORDING_ID';
-    print_r($client->deleteConferenceRecording($conference_id, $recording_id));
+    $recording_id = 123;
+    print_r($client->deleteConferenceRecording($existing_room_id, $recording_id));
     
+    // Chats
     print_r($client->chats());
     
-    print_r($client->conferenceSessionChats($session_id));
+    print_r($client->conferenceSessionChats($existing_session_id));
     
-    print_r($client->addFileLibraryFile('/path/to/file.png'));
-    
-    $params = array(
-    	'attendees' => array(
-    		array('email' => 'example@domain.com'),
-   		),
-    );
-    print_r($client->sendConferenceEmailInvitations($conference_id, 'us', $params));
-    
-    print_r($client->deleteConference($conference_id));
+    print_r($client->deleteConference($room_id));
 }
 catch (Exception $e)
 {
